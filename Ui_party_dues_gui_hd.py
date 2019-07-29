e380c3a625524a2487da48519cae5eff2bec8599
TimeCollector <xbx@cueb.edu.cn>
2019-07-29 10:11:17 +0800

    add icon

--- Ui_party_dues_gui_hd.py
+++ Ui_party_dues_gui_hd.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\workspace\cuebpartydues\party_dues_gui_hd.ui'
+# Form implementation generated from reading ui file 'g:\workspace\cuebpartydues\party_dues_gui_hd.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -14,7 +14,7 @@ class Ui_MainWindow(object):
         MainWindow.setEnabled(True)
         MainWindow.resize(581, 234)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../../Users/xbx/Desktop/party.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("party.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
